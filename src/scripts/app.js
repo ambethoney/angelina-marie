109b820075643ca39da7001eb2cdad033d03a172
Angelina Bethoney <ambethoney@yahoo.com>
2015-07-30 00:00:20 -0400

    redid front page, added canvas scripting, new layout

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,17 +1,28 @@
-m = require('mithril');
-v = require('velocity-animate');
+var m = require('mithril'),
 $ = require('jquery');
 
+require('../../node_modules/masonry-layout/masonry.js');
+// console.dir(require('../../node_modules/masonry-layout/masonry.js'))
 
 mysite = {};
 
-var url =   "../cockpit/rest/api/collections/get/"+ page +"?token=5e33744e5a42d77878db9c30 ";
+var url = "../cockpit/rest/api/collections/get/"+ page +"?token=5e33744e5a42d77878db9c30 ",
+    page;
 
 mysite.modules = {};
+
+mysite.subModules = {};
 
 mysite.anim = {};
 
 mysite.utils = {};
+
+
+mysite.utils.grid = function(el, init, context){
+    if(!init){
+        $(el).masonry()
+    }
+}
 
 mysite.anim.rollIn = function(el, init, context){
 
@@ -41,8 +52,8 @@ mysite.anim.easeNav = function(el, init, slide_up){
         })
 
         if(m.route() ==='/'){
-            $('nav').height(is_open);
-            $('.navigation').show();
+            $('nav').hide();
+            // $('.navigation').show();
         }
      }
 }
@@ -68,6 +79,42 @@ mysite.anim.pageSlideIn = function(el, init, context) {
     }
 }
 
+mysite.anim.greeting = function(el, init, context){
+
+    var canvas = document.getElementById("hello"),
+        ctx = canvas.getContext("2d"),
+        dashLen = 220, dashOffset = dashLen, speed = 5,
+        txt = "oh, hello!", x = canvas.width / 3.66, i = 0;
+
+    if(ctx != null){
+
+
+        ctx.font = "150px cursive, TSCu_Comic, sans-serif";
+        ctx.lineWidth = 5; ctx.lineJoin = "round"; ctx.globalAlpha = 2/3;
+        ctx.strokeStyle = ctx.fillStyle = "#99CC99";
+        ctx.textBaseline = "middle";
+
+        (function loop() {
+          ctx.clearRect(x, 0, 60, 150);
+          ctx.setLineDash([dashLen - dashOffset, dashOffset - speed]); // create a long dash mask
+          dashOffset -= speed;                                         // reduce dash length
+          ctx.strokeText(txt[i], x, 90);                               // stroke letter
+
+          if (dashOffset > 0) requestAnimationFrame(loop);             // animate
+          else {
+            ctx.fillText(txt[i], x, 90);                               // fill final letter
+            dashOffset = dashLen;                                      // prep next char
+            x += ctx.measureText(txt[i++]).width + ctx.lineWidth * Math.random();
+            ctx.setTransform(1, 0, 0, 1, 0, 3 * Math.random());        // random y-delta
+            ctx.rotate(Math.random() * 0.005);                         // random rotation
+            if (i < txt.length) requestAnimationFrame(loop);
+          }
+        })();
+    }
+
+
+
+}
 mysite.utils.submitForm = function(el, init, context){
 
     if(!init){
@@ -135,49 +182,109 @@ mysite.modules.Nav = {
 };
 
 mysite.modules.Page = function(subModule){
-    this.controller = subModule.controller,
-    this.view = function(ctrl){
-        return [ mysite.modules.Nav.view(), m(".page",[subModule.view()] ) ];
+    return {
+        controller: function() {
+            return {
+                subCtrl: new subModule.controller,
+
+            }
+        },
+        view : function(ctrl){
+            return [ mysite.modules.Nav.view(), m(".page",[subModule.view(ctrl.subCtrl)] ) ];
+        }
     }
 }
 
 
-mysite.modules.Home = new mysite.modules.Page({
+mysite.subModules.Home = {
     controller: function (){
-         console.log("yooo")
          var ctrl = this ;
     },
+
     view: function (ctrl){
 
         return[
             m("div.container#home",[
-                m("div.title",{config:mysite.anim.rollIn}, "angelina marie"),
-                m("div.subtitle", {config:mysite.anim.rollIn},"front end web developer")
+                m("canvas#hello",{
+                    "width":"1200px",
+                    "height":"300px",
+                    config:mysite.anim.greeting
+                }, "oh, hello!"),
+                m("div.title",{config:mysite.anim.rollIn}, "I'm Angelina"),
+                m("img[src='../img/about-img.jpg']",{class: "about-img",config:mysite.anim.rollIn}),
+                m("div.subtitle", {config:mysite.anim.rollIn},"I like to build interesting, thoughtful interfaces for businesses, non-profits, and you."),
+                m("div.home-nav",
+                    m("ul",{config:mysite.anim.rollIn},
+                        m("li", m("a[href='/about']", {config: m.route}, "Learn More", m('i',{class:'icon-arrow_right'}))),
+                        m("li", m("a[href='/portfolio']", {config: m.route}, "My Work", m('i',{class:'icon-arrow_right'}))),
+                        m("li", m("a[href='/contact']", {config: m.route}, "I'm Hireable!", m('i',{class:'icon-arrow_right'})))
+                    )
+                )
             ])
         ]
     }
-});
+};
 
 
-mysite.modules.About = new mysite.modules.Page({
+mysite.subModules.About ={
     controller: function (){
-        // console.log("yooo"),
          var me = this ;
     },
     view: function (ctrl){
-        // console.log(ctrl)
         return[
            m("div.container#about",[
                 m("div.page-title", "welcome! "),
                 m("div.left.bio", m.trust("<p>Hi, I'm Angelina. I've found myself in the Big Apple by way of Boston, Providence, and Charleston, SC. I take a lot of pride in building websites that <del> no one but me and my mom uses</del> promotes businesses, entertains, or lets me play with new technologies.</p>")),
-                m("img[src='../img/about-img.jpg']",{class: "about-img"}),
+
                 m("div.right.skills", m.trust("<p>What, you want to know more? When I finally pull away from my computer, I love arts & crafts of all varieties, lounging on the beach, and asking people what their new favorite library is at tech meetups across NYC.</p>"))
             ])
         ]
     }
-});
+};
 
-mysite.modules.Portfolio = new mysite.modules.Page({
+mysite.subModules.Portfolio ={
+
+
+    controller: function (){
+
+        var table = $('.table-portfolio');
+
+        var Project = function(data) {
+           data: m.prop(data)
+        }
+
+        Project.list = function() {
+            return m.request({
+                method: "GET",
+                url: "http://www.angelina-marie.com/cockpit/rest/api/collections/get/Portfolio?token=5e33744e5a42d77878db9c30"
+            });
+        }
+
+       this.projects = Project.list();
+
+    },
+
+    view : function (ctrl){
+         return[
+           m("div.container#portfolio",[
+                m("div.page-title", "portfolio"),
+                m("div.portfolio-sub"),
+                m("table.portfolio-table",
+                    m("tr",
+                        $.map(ctrl.projects(), function( val, i ) {
+                            return m("td",{config:mysite.anim.pageSlideIn},
+                                    m("img",{src: "http://www.angelina-marie.com/"+val.image[0].path.split(":").pop()+"", class:"portfolio-img" }),
+                                    m("span.portfolio-layer",m("span", val.title)))
+                        })
+                    )
+                )
+            ])
+        ]
+    }
+
+};
+
+mysite.subModules.Blog = {
 
     controller: function (){
 
@@ -211,57 +318,18 @@ mysite.modules.Portfolio = new mysite.modules.Page({
         ]
     }
 
-});
+};
 
-mysite.modules.Blog = new mysite.modules.Page({
 
+
+
+mysite.subModules.Contact = {
     controller: function (){
+         var me = this;
 
-        two = function(subject) {
-            var output = [];
-            for (var i = 0; i < 2; i++) output.push(subject(i));
-            return output;
-        };
-
-
-        four = function(subject) {
-            var output = [];
-            for (var i = 0; i < 4; i++) output.push(subject(i));
-            return output;
-        };
-    },
-
-
-    view: function (controller){
-
-         return[
-           m("div.container#portfolio",[
-                m("div.page-title", "portfolio"),
-                m("div.portfolio-sub", m.trust("I'm still working on this page! In the mean time, bask in the cuteness of the kittehs and check out my <a href='http://www.github.com/ambethoney'> github!</a>")),
-                 m("table.portfolio-table", two(function(){
-                    return m("tr", four(function(){
-                      return m("td",{config:mysite.anim.rollIn}, [m("img[src='http://placekitten.com/g/200/300']")])
-                    }))
-                 }))
-            ])
-        ]
-    }
-
-});
-
-
-
-
-
-
-
-mysite.modules.Contact = new mysite.modules.Page({
-    controller: function (){
-        // console.log("yooo"),
-         var me = this ;
     },
     view: function (ctrl){
-        // console.log(ctrl)
+
          return[
            m("div.container#contact",[
                 m("div.page-title", "I'm hireable!"),
@@ -331,17 +399,17 @@ mysite.modules.Contact = new mysite.modules.Page({
             ])
         ]
     }
-});
+};
 
 m.route.mode = "search";
 m.route(document.body, "/", {
-    "/": mysite.modules.Home,
-    "/about": mysite.modules.About,
-    "/portfolio": mysite.modules.Portfolio,
-    "/portfolio/:project": mysite.modules.Portfolio,
-    "/blog": mysite.modules.Blog,
-    "/blog/:post": mysite.modules.Blog,
-    "/contact": mysite.modules.Contact
+    "/": mysite.modules.Page(mysite.subModules.Home),
+    "/about": mysite.modules.Page(mysite.subModules.About),
+    "/portfolio": mysite.modules.Page(mysite.subModules.Portfolio),
+    "/portfolio/:project": mysite.modules.Page(mysite.subModules.Portfolio),
+    "/blog": mysite.modules.Page(mysite.subModules.Blog),
+    "/blog/:post": mysite.modules.Page(mysite.subModules.Blog),
+    "/contact": mysite.modules.Page(mysite.subModules.Contact),
 });
 
 
